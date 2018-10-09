d47edf720b5fd4e47a14f91079e9d8c27bcb7655
Luis Fernando Varela <lfvarela@stanford.edu>
2018-10-08 20:41:34 -0700

    Basic simulations working

--- simulations/baselines.py
+++ simulations/baselines.py
@@ -8,18 +8,20 @@ def sigmoid(x):
     :param x: Argument to the function
     :return: The sigmoid of x
     """
-    return 1 / (1 - np.exp(-x))
+    return 1 / (1 + np.exp(-x))
 
 
 def sigmoid_irf(a, c, d):
     """
 
-    :param theta:
-    :param c:
-    :param d:
-    :return:
+    :param a: ability
+    :param c: guess probability
+    :param d: difficulty
+    
+    :return: the probability that the student will get the question right
     """
     return c + (1 - c) * sigmoid(a - d)
+
 
 def floored_exp_irf(a, c, d):
     """
