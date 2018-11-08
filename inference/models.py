e1e4b94dfa382143a6c050d0719a2f71a240ca75
Alejandro Dobles <alejandro.d.96@gmail.com>
2018-11-07 19:10:37 -0800

    Major updates on code layout. New utils module takes care of common tasks.

--- inference/models.py
+++ inference/models.py
@@ -1,4 +1,6 @@
 import torch
+import torch.nn as nn
+import torch.nn.functional as f
 
 def two_param_sigmoid_irf(a, d, c):
     """
@@ -13,3 +15,73 @@ def two_param_sigmoid_irf(a, d, c):
 
 def floored_exp_irf(a, d, l, c):
     return torch.max(torch.tensor(c), 1 - torch.exp(-l * (a - d)))
+
+
+class Sigmoid_Model(nn.Module):
+    def __init__(self, num_students, num_questions, num_concepts, concepts, guess_prob=0.2):
+        """
+        Initializes a two parameter sigmoid IRF model.
+
+        :param num_students: The number of students per batch (usually the entire train set)
+        :param num_questions: The number of questions.
+        :param num_concepts: The number of concepts the questions test.
+        :param concepts: A tensor of shape (num_questions, num_concepts), where a 1 in the (i,j) entry implies that
+        question i tests concept j.
+        :param guess_prob: The probability that a student gets a question right by guessing.
+        """
+
+        super(Sigmoid_Model, self).__init__()
+        self.A = nn.Parameter(torch.randn(num_students, num_concepts), requires_grad=True)
+        _D = torch.zeros(num_questions, num_concepts)
+        _D[concepts] = torch.randn(num_questions)
+        self.D = nn.Parameter(_D, requires_grad=True)
+        self.concepts = concepts
+        self.guess_prob = guess_prob
+
+    def forward(self, x):
+        """
+        Calculates model predictions. Notice x, is not used. That is, our predictions don't depend on any input
+        features. This is because this is really an unsupervised learning problem, we only have access to unlabeled data
+        (the test results), and we are trying to infer matrices A and D from it, which we then use to
+        predict the test results.
+
+        :param x: Not used. Required by torch architecture.
+        :return: Predictions for the entire batch.
+        """
+        return two_param_sigmoid_irf(self.A[:, self.concepts[1]], self.D[self.concepts], self.guess_prob)
+
+
+class FE_Model(nn.Module):
+    def __init__(self, num_students, num_questions, num_concepts, concepts, guess_prob=0.2, l=10):
+        """
+        Initializes a floored exponential IRF model.
+
+        :param num_students: The number of students per batch (usually the entire train set)
+        :param num_questions: The number of questions.
+        :param num_concepts: The number of concepts the questions test.
+        :param concepts: A tensor of shape (num_questions, num_concepts), where a 1 in the (i,j) entry implies that
+        question i tests concept j.
+        :param guess_prob: The probability that a student gets a question right by guessing.
+        :param l:
+        """
+
+        super(FE_Model, self).__init__()
+        self.A = nn.Parameter(0.1 * torch.randn(num_students, num_concepts), requires_grad=True)
+        _D = torch.zeros(num_questions, num_concepts)
+        _D[concepts] = 0.1 * torch.randn(num_questions)
+        self.D = nn.Parameter(_D, requires_grad=True)
+        self.concepts = concepts
+        self.guess_prob = guess_prob
+        self.l = l
+
+    def forward(self, x):
+        """
+        Calculates model predictions. Notice x, is not used. That is, our predictions don't depend on any input
+        features. This is because this is really an unsupervised learning problem, we only have access to unlabeled data
+        (the test results), and we are trying to infer matrices A and D from it, which we then use to
+        predict the test results.
+
+        :param x: Not used. Required by torch architecture.
+        :return: Predictions for the entire batch.
+        """
+        return floored_exp_irf(self.A[:, self.concepts[1]], self.D[self.concepts], self.l, self.guess_prob)
---
+++ inference/utils.py
@@ -0,0 +1,123 @@
+import numpy as np
+import torch
+import torch.nn.functional as f
+import matplotlib.pyplot as plt
+
+def calc_acc(preds, targets):
+    return (targets.eq(preds.float()).sum().float() / (targets.shape[0] * targets.shape[1])).item()
+
+def calc_precision(preds, targets):
+    return ((targets.int() & preds).sum().float() / preds.sum().float()).item()
+
+def rmse(A, B):
+    return torch.sqrt(torch.sum((A - B)**2).float() / (A.shape[0] * A.shape[1])).item()
+
+def train(model, optim, epochs, Y):
+    """
+    General train sequence for model.
+
+    :param model: The model. Should inherit from nn.Module
+    :param optim: A torch.optim optimizer.
+    :param epochs: The number of epochs to train the model for.
+    :param Y: The data, a (num_students, num_questions) tensor.
+
+    :return: Three lists of length epochs holding the loss, accuracy and precision values for each epoch (on the train
+    set).
+    """
+    losses = []
+    train_acc = []
+    precision = []
+
+
+    for t in range(epochs):
+        # Forward pass: compute predicted y
+
+        scores = model(None)
+
+        # Compute and print loss
+        loss = f.binary_cross_entropy(scores, Y)
+
+        preds = (scores > 0.5).int()
+
+        losses.append(loss.item())
+        train_acc.append(calc_acc(preds, Y))
+        precision.append(calc_precision(preds, Y))
+
+        # We zero out gradients before optimization step.
+        optim.zero_grad()
+
+        # Backprop to compute gradients of params wrt loss
+        loss.backward()
+
+        # Update the parameters using the gradients computed.
+        optim.step()
+
+    return losses, train_acc, precision
+
+
+def sample(r):
+    if isinstance(r, tuple):
+        return 10**np.random.uniform(r[0], r[1])
+    return r
+
+
+def hyperparam_search(model_class, init_agrs, params, samples, epochs, Y):
+    """
+    Carries out random hyperparameter search on model.
+
+    :param model_class: The model class.
+    :param init_args: A tuple holding the arguments to initialize a new model of model_class.
+    :param params: A dict mapping hyperparameter to a (min exponent, max exponent) tuple, determining a range from which
+    to sample. Notice that the range is given as a range of exponents, this is so we get a unifrom distribution on the
+    exponents of the samples, instead of on the values of the parameters themselves.
+    Alternatively, keys can map to a single number if no tuning is needed on that hyperparameter. Valid parameters are
+    'lr', 'reg', 'momentum'.
+    :param samples: The number of samples to draw for each hyperparameter.
+    :param epochs: The number of epochs on which to train each candidate model. These should be small.
+    :param Y: The data set.
+
+    :return: The maximum accuracy achieved and a tuple holding the optimal parameters found.
+    """
+    lr_range = params.get('lr', 1e-3)
+    reg_range = params.get('reg', 1e-2)
+    m_range = params.get('momentum', 0)
+
+    n = int(isinstance(lr_range, tuple)) + int(isinstance(reg_range, tuple)) + int(isinstance(m_range, tuple))
+
+    param_candidates = [(sample(lr_range), sample(reg_range), sample(m_range)) for _ in range(n * samples)]
+
+    max_acc = 0
+    best_params = (0, 0, 0)
+
+    for candidate in param_candidates:
+        model = model_class(*init_agrs)
+        l, r, m = candidate
+        optim = torch.optim.SGD(model.parameters(), lr=l, weight_decay=r, momentum=m)
+        losses, train_acc, precision = train(model, optim, epochs, Y)
+        if train_acc[-1] > max_acc:
+            best_params = (l, r, m)
+            max_acc = train_acc[-1]
+
+    return max_acc, best_params
+
+
+
+def plot_train_metrics(losses, train_acc, precision):
+    """
+    Plots training metrics returned by train.
+
+    :param losses: list of losses by epoch.
+    :param train_acc: list of train accuracy by epoch.
+    :param precision: list of precision by epoch.
+    """
+    plt.figure(figsize=(10, 12))
+    plt.subplot(311)
+    plt.title('Loss')
+    plt.plot(losses, color='c')
+    plt.subplot(312)
+    plt.title('Accuracy')
+    plt.plot(train_acc, color='c')
+    plt.subplot(313)
+    plt.title('Precision')
+    plt.plot(precision, color='c')
+    plt.show()
